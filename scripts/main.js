7828a41ea0999ccdb4808c2906f314cba053ea7d
= <=>
2016-09-16 20:05:36 -0800

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+var Days = GetDays(),
+    selectedDate;
+
+function SortRows(a, b) {
+    a = new Date(a.Date);
+    b = new Date(b.Date);
+    return a<b ? -1 : a>b ? 1 : 0;
+}
+
+function SetTableCaption() {
+    var monthTable = document.getElementById("MonthView"),
+        tableCaption = monthTable.createCaption(),
+        monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        tableCaption.innerHTML = "<h3>" + monthNames[selectedDate.getMonth()] + "</h3>";
+}
+
+function BuildTable() {
+    var monthTable = document.getElementById("MonthView"),
+        tableBody = monthTable.getElementsByTagName("tbody")[0];
+
+    for (var i = 0; i < Days.length; i++)
+    {
+        var newRow = tableBody.insertRow(i),
+            dateCell = newRow.insertCell(0),
+            timesCell = newRow.insertCell(1),
+            restCell = newRow.insertCell(2),
+            oneThreeFiveCell = newRow.insertCell(3),
+            otherComCell = newRow.insertCell(4),
+            totalDaily = newRow.insertCell(5),
+            month = newRow.insertCell(6),
+            comments = newRow.insertCell(7),
+            editCell = newRow.insertCell(8),
+            dayDate = new Date(Days[i].Date);
+
+            dateCell.appendChild(document.createTextNode(dayDate.getDate()));
+    }
+}
+
+$(function() {
+    selectedDate = new Date();
+    SetTableCaption();
+    Days.sort(SortRows);
+    BuildTable();
+    $(".datepicker").datepicker({
+        onSelect: function(dateText, inst){
+            selectedDate = $(this).datepicker('getDate');
+            SetTableCaption();
+        }});
+    $(".datepicker").datepicker("setDate", new Date());
+    $(".timepicker").timepicker();
+});
---
+++ scripts/mocks.js
@@ -0,0 +1,9 @@
+function GetDays()
+{
+    return [
+        {"RowId": "1", "Date":"2016/09/01", "StartTime":"06:30", "EndTime":"12:30", "135Hours":"0", "Comments":"Gillnet survey/Thynes"},
+        {"RowId": "2", "Date":"2016/09/02", "StartTime":"08:00", "EndTime":"15:00", "135Hours":"0", "Comments":""},
+        {"RowId": "3", "Date":"2016/09/15", "StartTime":"09:00", "EndTime":"17:00", "135Hours":"0", "Comments":"Mountain Goat Transport"},
+        {"RowId": "4", "Date":"2016/09/07", "StartTime":"08:00", "EndTime":"12:30", "135Hours":"0", "Comments":"Stream Survey"}
+    ];
+}
